7c9437d4f02a80d59f4a1e7d53bd7b0036cd428d
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    a71-common: Patch liblearningmodule.so to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -53,6 +53,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
      'vendor/lib/libDualCamBokehCapture.camera.samsung.so': blob_fixup()
         .add_needed('libhigh_dynamic_range_bokeh.so'),
+     'vendor/lib64/liblearningmodule.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
      'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .sig_replace('E1 03 15 AA 08 00 40 F9 E3 03 14 AA 08 09 40 F9', 'E1 03 15 AA 08 00 40 F9 03 00 80 D2 08 09 40 F9'),
